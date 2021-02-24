c1053048da4ada3a9e67ea3469b64bdef685faaf
Yi Zhang <zhang9@outlook.de>
2021-02-24 23:32:54 +0100

    Initial commit

---
+++ VTI_2D.m
@@ -0,0 +1,345 @@
+function [v1,v3,R1,R3,P]=VTI_2D(dt,dx,dz,nt,nx,nz,...
+    X,Z,...
+    r1,r3,...
+    s1,s3,src1,src3,source_type,...
+    r1t,r3t, ...
+    s1t,s3t, ...
+    lp,nPML,R,...
+    C, ...
+    plot_interval,plot_source,...
+    path,...
+    save_wavefield)
+%% create folder for figures
+
+if ~exist(path,'dir')
+    mkdir(path)
+end
+
+n_picture=1;
+if save_wavefield==1
+    if ~exist([path '/forward_wavefield/'],'dir')
+        mkdir([path '/forward_wavefield/'])
+    end
+end
+
+if plot_interval~=0
+    if ~exist([path '/forward_pic/'],'dir')
+        mkdir([path '/forward_pic/'])
+    end
+end
+
+%% PML
+vmax=sqrt((C.C33)./C.rho);
+beta0=(ones(nx,nz,'single').*vmax*(nPML+1)*log(1/R)/2/lp/dx);
+beta1=(zeros(nx,nz,'single'));
+beta3=beta1;
+tt=(1:lp)/lp;
+tt2=repmat(reshape(tt,[lp,1]),[1,nz]);
+plane_grad1=zeros(nx,nz);
+plane_grad3=plane_grad1;
+
+plane_grad1(2:lp+1,:)=flip(tt2,1);
+plane_grad1(nx-lp:end-1,:)=tt2;
+plane_grad1(1,:)=plane_grad1(2,:);
+plane_grad1(end,:)=plane_grad1(end-1,:);
+
+tt2=repmat(reshape(tt,[1,lp]),[nx,1]);
+plane_grad3(:,2:lp+1)=flip(tt2,2);
+plane_grad3(:,nz-lp:end-1)=tt2;
+plane_grad3(:,1)=plane_grad3(:,2);
+plane_grad3(:,end)=plane_grad3(:,end-1);
+
+beta1=beta0.*plane_grad1.^nPML;
+beta3=beta0.*plane_grad3.^nPML;
+
+IND=unique(find(beta1.*beta3~=0));
+beta=beta1+beta3;
+beta(IND)=beta(IND)/2;
+clear beta1 beta3 plane_grad1 plane grad3 vmax
+%% source
+ind_sor=sub2ind([nx,nz],s1,s3);
+%% model configuration
+R1=(zeros(nt,length(r1),'single'));
+R3=(zeros(nt,length(r3),'single'));
+P=(zeros(nt,length(r3),'single'));
+ind_rec=sub2ind([nx,nz,3],r1,r3,ones(1,length(r1))*3);
+
+%% monoclinic 2D solver xz plane (symmetric plane)
+v1=(zeros(nx,nz,3,'single'));
+v3=v1;
+lim2=(zeros(4,2,'single'));
+
+sigmas11=(zeros(nx,nz,'single'));
+sigmas13=sigmas11;
+sigmas33=sigmas11;
+p=sigmas11;
+ts=sigmas11;
+ts2=sigmas11;
+
+l=1;
+ts(ind_sor)=src1(1,:);
+ts2(ind_sor)=src3(1,:);
+
+switch source_type
+    case 'D'
+        v1(:,:,3)=v1(:,:,3)+.5./C.rho.*ts;
+        v3(:,:,3)=v3(:,:,3)+.5./C.rho.*ts2;
+    case 'P'
+        v1(:,:,3)=v1(:,:,3)+.5./C.rho.*D(ts,-1)/dx;
+        v3(:,:,3)=v3(:,:,3)+.5./C.rho.*D(ts2,-3)/dz;
+end
+%% save wavefield
+if save_wavefield==1
+    data=v1(:,:,2);
+    save([path '/forward_wavefield/v1_' num2str(l) '.mat'],'data');
+    data=v3(:,:,2);
+    save([path '/forward_wavefield/v3_' num2str(l) '.mat'],'data');
+    data=sigmas11;
+    save([path '/forward_wavefield/sigma11_' num2str(l) '.mat'],'data');
+    data=sigmas33;
+    save([path '/forward_wavefield/sigma33_' num2str(l) '.mat'],'data');
+    data=sigmas13;
+    save([path '/forward_wavefield/sigma13_' num2str(l) '.mat'],'data');
+    data=p;
+    save([path '/forward_wavefield/p_' num2str(l) '.mat'],'data');
+    
+    data=v1(:,:,3);
+    save([path '/forward_wavefield/v1_' num2str(l+1) '.mat'],'data');
+    data=v3(:,:,3);
+    save([path '/forward_wavefield/v3_' num2str(l+1) '.mat'],'data');
+    data=sigmas11;
+    save([path '/forward_wavefield/sigma11_' num2str(l+1) '.mat'],'data');
+    data=sigmas33;
+    save([path '/forward_wavefield/sigma33_' num2str(l+1) '.mat'],'data');
+    data=sigmas13;
+    save([path '/forward_wavefield/sigma13_' num2str(l+1) '.mat'],'data');
+    data=p;
+    save([path '/forward_wavefield/p_' num2str(l+1) '.mat'],'data');
+end
+%%
+tic;
+for l=2:nt-1
+    for l2=1:2
+        v1(:,:,l2)=v1(:,:,l2+1);
+        v3(:,:,l2)=v3(:,:,l2+1);
+    end
+    %% compute sigma
+    v1_x=D(v1(:,:,2),1)/dx;
+    v3_x=D(v3(:,:,2),-1)/dx;
+    v1_z=D(v1(:,:,2),-3)/dz;
+    v3_z=D(v3(:,:,2),3)/dz;
+    
+    v1_x2=D(v1(:,:,1),1)/dx;
+    v3_x2=D(v3(:,:,1),-1)/dx;
+    v1_z2=D(v1(:,:,1),-3)/dz;
+    v3_z2=D(v3(:,:,1),3)/dz;
+    
+    
+    sigmas11=dt*.5*((C.C11-C.C13).*v1_x...
+        +(C.C13-C.C33).*v3_z)...
+        +sigmas11...
+        -dt*beta.*sigmas11;
+    
+    sigmas33=dt*.5*((C.C33-C.C13).*v3_z...
+        +(C.C13-C.C11).*v1_x)...
+        +sigmas33...
+        -dt*beta.*sigmas33;
+    
+    sigmas13=dt*(C.C55.*(v1_z+v3_x))...
+        +sigmas13...
+        -dt*beta.*sigmas13;
+    %% p
+    p=-dt*((C.C11+C.C33)*.5.*v1_x+(C.C13+C.C33)*.5.*v3_z) ...
+        +p ...
+        -dt*beta.*p;
+    %%
+    ts(ind_sor)=src1(l,:);
+    ts2(ind_sor)=src3(l,:);
+    %{
+    switch source_type
+        case 'D'
+            p=p+1./C.rho.*ts2;
+    end
+    %}
+    %% compute v
+    v1(:,:,3)=dt./C.rho.*(D(sigmas11-p,-1)/dx...
+        +D(sigmas13,3)/dz)...
+        +v1(:,:,3)...
+        -dt*beta.*v1(:,:,2);
+    v3(:,:,3)=dt./C.rho.*(D(sigmas13,1)/dx...
+        +D(sigmas33-p,-3)/dz)...
+        +v3(:,:,3)...
+        -dt*beta.*v3(:,:,2);
+    
+    
+    ts(ind_sor)=src1(l,:);
+    ts2(ind_sor)=src3(l,:);
+    
+    switch source_type
+        case 'D'
+            v1(:,:,3)=v1(:,:,3)+1./C.rho.*ts;
+            v3(:,:,3)=v3(:,:,3)+1./C.rho.*ts2;
+        case 'P'
+            v1(:,:,3)=v1(:,:,3)+1./C.rho.*D(ts,-1)/dx;
+            v3(:,:,3)=v3(:,:,3)+1./C.rho.*D(ts2,-3)/dz;
+    end
+    %% fixed boundary condition
+    v1(1,:,3)=0;
+    v1(end,:,3)=0;
+    v1(:,1,3)=0;
+    v1(:,end,3)=0;
+    
+    v3(1,:,3)=0;
+    v3(end,:,3)=0;
+    v3(:,1,3)=0;
+    v3(:,end,3)=0;
+    %% assign recordings
+    R1(l+1,:)=v1(ind_rec);
+    R3(l+1,:)=v3(ind_rec);
+    P(l+1,:)=p(ind_rec-nx*nz*2);
+    %% save wavefield
+    if save_wavefield==1
+        
+        data=v1(:,:,3);
+        save([path '/forward_wavefield/v1_' num2str(l+1) '.mat'],'data');
+        data=v3(:,:,3);
+        save([path '/forward_wavefield/v3_' num2str(l+1) '.mat'],'data');
+        data=sigmas11;
+        save([path '/forward_wavefield/sigma11_' num2str(l+1) '.mat'],'data');
+        data=sigmas33;
+        save([path '/forward_wavefield/sigma33_' num2str(l+1) '.mat'],'data');
+        data=sigmas13;
+        save([path '/forward_wavefield/sigma13_' num2str(l+1) '.mat'],'data');
+        data=p;
+        save([path '/forward_wavefield/p_' num2str(l+1) '.mat'],'data');
+    end
+    %% plot
+    if plot_interval~=0
+        if mod(l,plot_interval)==0 || l==nt-1
+            lim2(1,1)=min(min(v3,[],'all'),lim2(1,1));
+            lim2(1,2)=max(max(v3,[],'all'),lim2(1,2));
+            lim2(2,1)=min(min(sigmas33,[],'all'),lim2(2,1));
+            lim2(2,2)=max(max(sigmas33,[],'all'),lim2(2,2));
+            lim2(3,1)=min(min(sigmas13,[],'all'),lim2(3,1));
+            lim2(3,2)=max(max(sigmas13,[],'all'),lim2(3,2));
+            lim2(4,1)=min(min(p,[],'all'),lim2(4,1));
+            lim2(4,2)=max(max(p,[],'all'),lim2(4,2));
+            
+            
+            hfig=figure('Visible','off');
+            set(gcf,'position',[80,80,1300,600]);
+            subplot(2,3,1)
+            imagesc(X,Z,v3(:,:,3)');
+            set(gca,'ydir','normal');
+            colorbar;
+            xlabel({['x [m]']});
+            ylabel({['z [m]']});
+            title({['t=' num2str(l*dt) 's'],['v_3 [m/s]']});
+            xlabel('x [m]');
+            ylabel('z [m]');
+            colorbar;
+            hold on;
+            if plot_source==1
+                for i=1:length(s1)
+                    ax2=plot(s1t(i),s3t(i),'v','color',[1,0,0]);
+                    hold on;
+                end
+            end
+            for i=1:length(r1)
+                ax4=plot(r1t(i),r3t(i),'^','color',[0,1,1]);
+                hold on;
+            end
+            ax3=plot(X(1)+[lp+1,lp+1]*dx,Z(1)-[lp+1,nz-lp-1]*dz,'color','blue');
+            hold on;
+            ax3=plot(X(1)+[nx-lp-1,nx-lp-1]*dx,Z(1)-[lp+1,nz-lp-1]*dz,'color','blue');
+            hold on;
+            hold on;
+            ax3=plot(X(1)+[lp+1,nx-lp-1]*dx,Z(1)-[nz-lp-1,nz-lp-1]*dz,'color','blue');
+            axis on;
+            ax3=plot(X(1)+[lp+1,nx-lp-1]*dx,Z(1)-[lp+1,lp+1]*dz,'color','blue');
+            axis on;
+            
+            subplot(2,3,2)
+            imagesc(X,Z,sigmas33');
+            set(gca,'ydir','normal');
+            xlabel('x [m]');
+            ylabel('z [m]');
+            title('\sigma_{s33} [Pa]');
+            colorbar;
+            
+            
+            subplot(2,3,3)
+            imagesc(X,Z,sigmas13');
+            set(gca,'ydir','normal');
+            xlabel('x [m]');
+            ylabel('z [m]');
+            title('\sigma_{s13} [Pa]');
+            colorbar;
+            
+            subplot(2,3,4)
+            imagesc(X,Z,p');
+            set(gca,'ydir','normal');
+            xlabel('x [m]');
+            ylabel('z [m]');
+            title('p [Pa]');
+            colorbar;
+            
+            subplot(2,3,5)
+            imagesc([1,length(r1)],[1,(l+1)]*dt,R3(1:l+1,:));
+            colorbar;
+            xlabel('Nr');
+            ylabel('t [s]');
+            title('R3 [m/s]');
+            ylim([1,nt]*dt);
+            
+            subplot(2,3,6)
+            imagesc(X,Z,C.C33');
+            set(gca,'ydir','normal');
+            xlabel('x [m]');
+            ylabel('z [m]');
+            title('C33 [Pa]');
+            colorbar;
+            hold on;
+            if plot_source==1
+                for i=1:length(s1)
+                    ax2=plot(s1t(i),s3t(i),'v','color',[1,0,0]);
+                    hold on;
+                end
+            end
+            for i=1:length(r1)
+                ax4=plot(r1t(i),r3t(i),'^','color',[0,1,1]);
+                hold on;
+            end
+            ax3=plot(X(1)+[lp+1,lp+1]*dx,Z(1)-[lp+1,nz-lp-1]*dz,'color','blue');
+            hold on;
+            ax3=plot(X(1)+[nx-lp-1,nx-lp-1]*dx,Z(1)-[lp+1,nz-lp-1]*dz,'color','blue');
+            hold on;
+            hold on;
+            ax3=plot(X(1)+[lp+1,nx-lp-1]*dx,Z(1)-[nz-lp-1,nz-lp-1]*dz,'color','blue');
+            axis on;
+            ax3=plot(X(1)+[lp+1,nx-lp-1]*dx,Z(1)-[lp+1,lp+1]*dz,'color','blue');
+            axis on;
+            if plot_source==1
+                legend([ax2,ax3,ax4],...
+                    'source','PML boundary','receiver',...
+                    'Location',[0.5,0.02,0.005,0.002],'orientation','horizontal');
+            else
+                legend([ax3,ax4],...
+                    'PML boundary','receiver',...
+                    'Location',[0.5,0.02,0.005,0.002],'orientation','horizontal');
+            end
+            
+            print(gcf,[path './forward_pic/' num2str(n_picture) '.png'],'-dpng','-r200');
+            n_picture=n_picture+1;
+            
+        end
+    end
+    
+    fprintf('\n time step=%d/%d',l+1,nt);
+    fprintf('\n    epalsed time=%.2fs',toc);
+    fprintf('\n    n_picture=%d',n_picture);
+    d=clock;
+    fprintf('\n    current time=%d %d %d %d %d %.0d',d(1),d(2),d(3),d(4),d(5),d(6));
+    
+end
+%%
---
+++ input_forward_template.m
@@ -0,0 +1,190 @@
+%% input
+close all;
+clear all;
+
+% load image
+vp=ones(6000,6000)*2000;
+
+% dimensions
+dt=10^-3;
+dx=10;
+dz=10;
+nt=1000;
+nx=size(vp,1);
+nz=size(vp,2);
+
+X=[0,dx*nx];
+Z=[0,-dz*nz];
+
+% PML layers
+lp=1;
+
+% PML coefficient, usually 2
+nPML=2;
+
+% Theoretical coefficient, more PML layers, less R
+% Empirical values
+% lp=[10,20,30,40]
+% R=[.1,.01,.001,.0001]
+R=1;
+
+% generate empty density
+rho=ones(nx,nz)*1;
+%rho=rho.*A;
+% Lame constants for solid
+mu=rho.*(vp/sqrt(3)).^2;
+lambda=rho.*vp.^2;
+% lambda(:,1:29)=0;
+
+C.C11=lambda+2*mu;
+C.C13=lambda;
+C.C33=lambda+2*mu;
+C.C55=mu;
+C.rho=rho;
+%%
+% find surrounding layers
+[a1,b1]=meshgrid(1:lp+2,1:nz);
+[a2,b2]=meshgrid(nx-(lp+2)+1:nx,1:nz);
+[a3,b3]=meshgrid(1:nx,1:lp+2);
+[a4,b4]=meshgrid(1:nx,nz-(lp+2)+1:nz);
+IND_air_layer=sub2ind([nx,nz],[reshape(a1,[1,(lp+2)*nz]),reshape(a2,[1,(lp+2)*nz]),reshape(a3,[1,(lp+2)*nx]),reshape(a4,[1,(lp+2)*nx])],...
+    [reshape(b1,[1,(lp+2)*nz]),reshape(b2,[1,(lp+2)*nz]),reshape(b3,[1,(lp+2)*nx]),reshape(b4,[1,(lp+2)*nx])]);
+clear a1 a2 a3 a4 b1 b2 b3 b4
+% assign surrounding layers with air
+%{
+C11(IND_air_layer)=1145*340^2;
+C13(IND_air_layer)=1145*340^2;
+C15(IND_air_layer)=0;
+C33(IND_air_layer)=1145*340^2;
+C35(IND_air_layer)=0;
+C55(IND_air_layer)=0;
+rho(IND_air_layer)=1145;
+%}
+
+
+% source
+% magnitude
+M=2.7;
+
+s_s1=[fix(nx/2)];
+s_s3=ones(size(s_s1))*fix(nz/2);
+%%
+%{
+figure;
+set(gcf,'position',[80,80,1800,900]);
+imagesc(X,Z,C.C55');
+colorbar;
+hold on
+ax=plot(dx*s_s1,-dz*s_s3,'v','color',[1,0,0]);
+set(gca,'ydir','normal');
+hold on;
+ax2=plot(dx*r1,-dz*r3,'^','color',[0,1,1]);
+hold on;
+colorbar;
+xlabel('x [m]');
+ylabel('z [m]');
+title('C55 [Pa]');
+legend([ax,ax2],...
+    'source','receiver', ...
+    'Location',[0.5,0.02,0.005,0.002],'orientation','horizontal');
+print(gcf,['C55.png'],'-dpng','-r200');
+%}
+%%
+% point interval in time steps
+plot_interval=100;
+
+p2=mfilename('fullpath');
+if ~exist(p2,'dir')
+    mkdir(p2)
+end
+tic;
+for source_code=1:length(s_s1)
+    % source locations
+    s1=s_s1(source_code);
+    s3=s_s3(source_code);
+    
+    % source frequency [Hz]
+    freq=5;
+    
+    % source signal
+    singles=rickerWave(freq,dt,nt,M);
+    
+    % give source signal to x direction
+    src1=zeros(nt,1);
+    src1=0*repmat(singles,[1,length(s3)]);
+    
+    % give source signal to z direction
+    src3=src1;
+    src3=1*repmat(singles,[1,length(s3)]);
+    
+    % receiver locations [m]
+    %r1=[22:5:nx-22,22:5:nx-22,ones(size(22:5:nz-22))*25,ones(size(22:5:nz-22))*120];
+    %r3=[ones(size(22:5:nx-22))*25,ones(size(22:5:nx-22))*80,22:5:nz-22,22:5:nz-22];
+    r1=[4];
+    r3=[4];
+    
+    s1t=dx*s1;
+    s3t=-dz*s3;
+    r1t=dx*r1;
+    r3t=-dz*r3;
+    
+    % source type. 'D' for directional source. 'P' for P-source.
+    source_type='D';
+    
+    
+    % plot source
+    plot_source=1;
+    
+    % figure path
+    path=[p2 '/source_code_' num2str(source_code) '/'];
+    
+    % save wavefield
+    save_wavefield=0;
+    %% pass parameters to solver
+    [v1,v3,R1,R3,P]=VTI_2D(dt,dx,dz,nt,nx,nz,...
+        X,Z,...
+        r1,r3,...
+        s1,s3,src1,src3,source_type,...
+        r1t,r3t, ...
+        s1t,s3t, ...
+        lp,nPML,R,...
+        C, ...
+        plot_interval,plot_source,...
+        path,...
+        save_wavefield);
+    %{
+    %% write to gif
+    sources=[path 'pic/'];
+    delaytime=.2;
+    filename=['animation'];
+    gifmaker(filename,delaytime,sources);
+        %}
+        %% write recordings
+        %% recording saving
+        if ~exist([path '/rec/'],'dir')
+            mkdir([path '/rec/'])
+        end
+        
+        DATA=rec_conversion(nt,nx,nz,dt,dx,dz,s1,s3,r1,r3);
+        parsave([path '/rec/simu_info.mat'],DATA);
+        
+        DATA=R1;
+        parsave([path '/rec/tR1.mat'],DATA);
+        DATA=R3;
+        parsave([path '/rec/tR3.mat'],DATA);
+        DATA=P;
+        parsave([path '/rec/tP.mat'],DATA);
+        %% source saving
+        %% recording saving
+        if ~exist([path '/source/'],'dir')
+            mkdir([path '/source/'])
+        end
+        DATA=src1;
+        parsave([path '/source/src1.mat'],DATA);
+        DATA=src3;
+        parsave([path '/source/src3.mat'],DATA);
+        DATA=source_type;
+        parsave([path '/source/source_type.mat'],DATA);
+end
+toc;
+%%
